0e4d231e887097e8270ea79323d8aabb012b1fbc
Henrique Melo <hmelo2509@gmail.com>
2022-03-25 20:44:12 +0000

    Parte 1 do curso

---
+++ src/main/java/br/com/alura/servletcourse/Empresa.java
@@ -0,0 +1,34 @@
+package br.com.alura.servletcourse;
+
+import java.util.Date;
+
+public class Empresa {
+	private static int implement_id = 0; 
+	
+	private Integer id;
+	private String nome;
+	private Date dataAbertura = new Date();
+	
+	public Empresa() {
+		this.id = implement_id;
+		implement_id++;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+}
